7298f3ecd1187c272f4a2fd1c0507259eebb023c
Max <max.09@outlook.xom>
2020-04-18 09:00:16 +0200

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { LargeCircleButtonComponent } from './large-circle-button/large-circle-button.component';
@@ -18,7 +18,9 @@ import { LargeCircleButtonComponent } from './large-circle-button/large-circle-b
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
     NbEvaIconsModule,
-    AppRoutingModule  ],
+    AppRoutingModule,
+    NbButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/large-circle-button/large-circle-button.component.ts
+++ src/app/large-circle-button/large-circle-button.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-large-circle-button',
   template: `
-    <p>
-      large-circle-button works!
-      <button class="btn-circle">50%</button>
 
-    </p>
+      <button nbButton shape="round" size="giant" appearance="outline">{{text}}</button>
   `,
   styleUrls: ['./large-circle-button.component.scss']
 })
 export class LargeCircleButtonComponent implements OnInit {
+  @Input() text: string;
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
 
 }
