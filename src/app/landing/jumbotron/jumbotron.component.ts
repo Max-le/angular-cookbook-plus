5847e668a423da1b5c5081ebb574cf3013c5d4f7
Max <max.09@outlook.com>
2020-04-19 14:49:48 +0200

    add buttons to jumbotron

--- src/app/landing/jumbotron/jumbotron.component.ts
+++ src/app/landing/jumbotron/jumbotron.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-jumbotron',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
       <h1>Welcome to CookBook Plus.</h1>
       <h2>CookBook+ is a convenient virtual cooking assistant.</h2>
       <hr>
-      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p>Save and organize your favorite recipes, find some refreshing ideas, and much more !</p>
       <p>
+        <button appearance="outline" shape="round" nbButton>Learn more</button>
+        <button (click)="redirect()" appearance="outline" shape="round" nbButton>Login</button>
+        <button appearance="outline" shape="round" nbButton>Register</button>
         <button appearance="outline" shape="round" nbButton>Learn more</button>
       </p>
   `,
@@ -16,9 +20,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JumbotronComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) {}
+
 
   ngOnInit(): void {
+  }
+  redirect(){
+    // TODO : make this a function w parameter to handle all routes.
+    this.router.navigateByUrl('/login');
   }
 
 }
