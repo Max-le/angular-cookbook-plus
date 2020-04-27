f9f243635b82a205e629fe47fbe9a8f83e98c279
Max <max.09@outlook.xom>
2020-04-27 11:10:27 +0200

    remove old imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
 
-import {LoginComponent} from './auth/login/login.component';
-import {RegisterComponent} from './auth/register/register.component';
+
 import {DashboardComponent} from './dashboard/dashboard.component';
 
 import {
