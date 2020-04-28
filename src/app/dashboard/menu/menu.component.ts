b41c83d2d24c1999fc077ba54db1b19811bd0d33
Max <max.09@outlook.xom>
2020-04-28 14:36:04 +0200

    clean up menu

--- src/app/dashboard/menu/menu.component.ts
+++ src/app/dashboard/menu/menu.component.ts
@@ -14,10 +14,27 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
   }
   items: NbMenuItem[] =  [
+
+    {
+      title: 'Home',
+      link:'/',
+    },
     {
       title: 'Profile',
       icon: 'person-outline',
-    }
-  ]
+    },
+    {
+      title: 'Change Password',
+      icon: 'lock-outline',
+    },
+    {
+      title: 'Privacy Policy',
+      icon: { icon: 'checkmark-outline', pack: 'eva' },
+    },
+    {
+      title: 'Logout',
+      icon: 'unlock-outline',
+    },
+  ];
 
 }
