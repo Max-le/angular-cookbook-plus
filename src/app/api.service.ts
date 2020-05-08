01761e16285387c9d84918a653d13483217bfe5d
Max Lepin <max.09@outlook.com>
2020-05-08 18:41:45 +0200

    edit provider "range"

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient } from '@angular/common/http';
 @Injectable({
+  // If value is DashboardModule, This service will be only available when the dashboard is loadded is the app.
   providedIn: 'root'
 })
 export class ApiService {
